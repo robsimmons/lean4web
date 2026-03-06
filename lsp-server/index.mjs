e853b9befdb0dc9f23aa9fd0c0dfd289c25acfeb
Rob Simmons <rob@lean-fro.org>
2026-03-06 12:46:32 -0500

    actually use cache cleverly

--- lsp-server/index.mjs
+++ lsp-server/index.mjs
@@ -1,29 +1,31 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from 'ws'
 import express from 'express'
-import * as cp from 'child_process';
-import * as url from 'url';
-import * as rpc from 'vscode-ws-jsonrpc';
+import * as cp from 'child_process'
+import * as url from 'url'
+import * as rpc from 'vscode-ws-jsonrpc'
 import * as path from 'path'
-import * as jsonrpcserver from 'vscode-ws-jsonrpc/server';
-import { stat, mkdir, mkdtemp, unlink, writeFile } from 'fs/promises';
+import * as jsonrpcserver from 'vscode-ws-jsonrpc/server'
+import { stat, mkdir, mkdtemp, unlink, writeFile } from 'fs/promises'
 import { randomUUID } from 'node:crypto'
-import { env } from 'process';
+import { env } from 'process'
 import nocache from 'nocache'
 import anonymize from 'ip-anonymize'
 import os from 'os'
 import http from 'http'
 import https from 'https'
-import { createSession, getSession, OUTPUT_ROOT_DIR, sendToSession } from './outputdir.mjs';
+import { createSession, getSession, OUTPUT_ROOT_DIR, sendToSession } from './outputdir.mjs'
 
 let socketCounter = 0
 
 function logStats() {
   console.log(`[${new Date()}] Number of open sockets - ${socketCounter}`)
-  console.log(`[${new Date()}] Free RAM - ${Math.round(os.freemem() / 1024 / 1024)} / ${Math.round(os.totalmem() / 1024 / 1024)} MB`)
+  console.log(
+    `[${new Date()}] Free RAM - ${Math.round(os.freemem() / 1024 / 1024)} / ${Math.round(os.totalmem() / 1024 / 1024)} MB`,
+  )
 }
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 const environment = process.env.NODE_ENV
 const isGithubAction = process.env.GITHUB_ACTIONS
@@ -62,6 +64,22 @@ app.use('/api/session', (req, res) => {
 app.get('/verso', (_req, res) => {
   res.send('')
 })
+app.use('/verso/view/:id/html-single/-verso-search', (_req, res, next) => {
+  res.set('Cache-Control', 'no-transform, max-age=31536000')
+  next()
+})
+app.use('/verso/view/:id/html-single/-verso-data', (_req, res, next) => {
+  res.set('Cache-Control', 'no-transform, max-age=31536000')
+  next()
+})
+app.use('/verso/view/:id/html-single/book.css', (_req, res, next) => {
+  res.set('Cache-Control', 'no-transform, max-age=31536000')
+  next()
+})
+app.use('/verso/view/:id/html-single/verso-vars.css', (_req, res, next) => {
+  res.set('Cache-Control', 'no-transform, max-age=31536000')
+  next()
+})
 app.use('/verso/view', express.static(OUTPUT_ROOT_DIR))
 // Using the client files
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')))
@@ -69,22 +87,22 @@ app.use(nocache())
 
 let server
 if (crtFile && keyFile) {
-  var privateKey  = fs.readFileSync(keyFile, 'utf8');
-  var certificate = fs.readFileSync(crtFile, 'utf8');
-  var credentials = {key: privateKey, cert: certificate};
+  var privateKey = fs.readFileSync(keyFile, 'utf8')
+  var certificate = fs.readFileSync(crtFile, 'utf8')
+  var credentials = { key: privateKey, cert: certificate }
 
   const PORT = process.env.PORT ?? 443
-  server = https.createServer(credentials, app).listen(PORT,
-    () => console.log(`HTTPS on port ${PORT}`));
+  server = https
+    .createServer(credentials, app)
+    .listen(PORT, () => console.log(`HTTPS on port ${PORT}`))
 
   // redirect http to https
-  express().get('*', function(req, res) {
-    res.redirect('https://' + req.headers.host + req.url).listen(80);
+  express().get('*', function (req, res) {
+    res.redirect('https://' + req.headers.host + req.url).listen(80)
   })
 } else {
   const PORT = process.env.PORT ?? 8080
-  server = app.listen(PORT,
-    () => console.log(`HTTP on port ${PORT}`))
+  server = app.listen(PORT, () => console.log(`HTTP on port ${PORT}`))
 }
 
 const wss = new WebSocketServer({ server })
@@ -100,29 +118,29 @@ async function startServerProcess(project, sessionId) {
   let projectPath = path.join(projectsBasePath, project)
   // Check for presence of directory
   try {
-    await stat(path.join(projectPath, "lean-toolchain"))
+    await stat(path.join(projectPath, 'lean-toolchain'))
   } catch (err) {
     console.log(`Could not find project (${projectPath})`)
-    return null;
+    return null
   }
 
-  let session = getSession(sessionId);
+  let session = getSession(sessionId)
 
   let serverProcess
   if (isDevelopment) {
     if (!isGithubAction) {
-      console.warn("Running without Bubblewrap container!")
+      console.warn('Running without Bubblewrap container!')
     }
-    serverProcess = cp.spawn("lake", ["serve", "--"], { 
+    serverProcess = cp.spawn('lake', ['serve', '--'], {
       cwd: projectPath,
-      env: { ...process.env, VERSO_OUTPUT_PATH: session?.mainDir }
+      env: { ...process.env, VERSO_OUTPUT_PATH: session?.mainDir },
     })
   } else {
     // the nix setup runs the server in ~,
     // `bubblewrap.sh` is somewhere relative to the js source file
     // and the projects are in ~/deploy/live/<project>
-    let cmd = path.join (__dirname, "bubblewrap.sh");
-    let cmdArgs = [projectPath].concat(session ? [session.mainDir] : []) ;
+    let cmd = path.join(__dirname, 'bubblewrap.sh')
+    let cmdArgs = [projectPath].concat(session ? [session.mainDir] : [])
     console.info(`Running with Bubblewrap container: ${cmd} ${cmdArgs}.`)
     serverProcess = cp.spawn(cmd, cmdArgs, {})
   }
@@ -131,17 +149,13 @@ async function startServerProcess(project, sessionId) {
   //   console.log(`Lean Server: ${data}`);
   // });
 
-  serverProcess.stderr.on('data', data =>
-    console.error(`Lean Server: ${data}`)
-  )
+  serverProcess.stderr.on('data', (data) => console.error(`Lean Server: ${data}`))
 
-  serverProcess.on('error', error =>
-    console.error(`Launching Lean Server failed: ${error}`)
-  )
+  serverProcess.on('error', (error) => console.error(`Launching Lean Server failed: ${error}`))
 
   serverProcess.on('close', (code) => {
-    console.log(`lean server exited with code ${code}`);
-  });
+    console.log(`lean server exited with code ${code}`)
+  })
 
   return serverProcess
 }
@@ -158,11 +172,11 @@ function urisToFilenames(prefix, obj) {
         obj[key] = path.join(prefix, obj[key])
       }
       if (typeof obj[key] === 'object' && obj[key] !== null) {
-        urisToFilenames(prefix, obj[key]);
+        urisToFilenames(prefix, obj[key])
       }
     }
   }
-  return obj;
+  return obj
 }
 
 /** Transform server file back into client URI */
@@ -173,37 +187,54 @@ function FilenamesToUri(prefix, obj) {
         obj[key] = obj[key].replace(prefix, '')
       }
       if (typeof obj[key] === 'object' && obj[key] !== null) {
-        FilenamesToUri(prefix, obj[key]);
+        FilenamesToUri(prefix, obj[key])
       }
     }
   }
-  return obj;
+  return obj
 }
 
-wss.addListener("connection", async function(ws, req) {
+wss.addListener('connection', async function (ws, req) {
   const urlRegEx = /^\/websocket\/([\w.-]+)(\?id=([a-f0-9-]+))?$/
   const reRes = urlRegEx.exec(req.url)
-  if (!reRes) { console.error(`Connection refused because of invalid URL: ${req.url}`); return; }
+  if (!reRes) {
+    console.error(`Connection refused because of invalid URL: ${req.url}`)
+    return
+  }
   const project = reRes[1]
   const sessionId = reRes[3]
-  if (!project.match(/^[a-zA-Z][a-zA-Z1-9.-_]*/)) { console.error(`Connection refused because of invalid project name: ${project}`); return; }
+  if (!project.match(/^[a-zA-Z][a-zA-Z1-9.-_]*/)) {
+    console.error(`Connection refused because of invalid project name: ${project}`)
+    return
+  }
 
   const ip = anonymize(req.headers['x-forwarded-for'] || req.socket.remoteAddress)
   const ps = await startServerProcess(project, sessionId)
-  if (ps === null) { console.error(`Connection refused because of nonexistent project directory: ${project}`); return; }
+  if (ps === null) {
+    console.error(`Connection refused because of nonexistent project directory: ${project}`)
+    return
+  }
 
   const socket = {
-      onMessage: (cb) => { ws.on("message", cb) },
-      onError: (cb) => { ws.on("error", cb) },
-      onClose: (cb) => { ws.on("close", cb) },
-      send: (data, cb) => { ws.send(data,cb) }
+    onMessage: (cb) => {
+      ws.on('message', cb)
+    },
+    onError: (cb) => {
+      ws.on('error', cb)
+    },
+    onClose: (cb) => {
+      ws.on('close', cb)
+    },
+    send: (data, cb) => {
+      ws.send(data, cb)
+    },
   }
   const reader = new rpc.WebSocketMessageReader(socket)
   const writer = new rpc.WebSocketMessageWriter(socket)
   const socketConnection = jsonrpcserver.createConnection(reader, writer, () => ws.close())
   const serverConnection = jsonrpcserver.createProcessStreamConnection(ps)
-  socketConnection.forward(serverConnection, message => {
-    const prefix = isDevelopment ? projectsBasePath : ""
+  socketConnection.forward(serverConnection, (message) => {
+    const prefix = isDevelopment ? projectsBasePath : ''
 
     if (!message.method === 'textDocument/definition') {
       urisToFilenames(prefix, message)
@@ -212,18 +243,18 @@ wss.addListener("connection", async function(ws, req) {
     if (isDevelopment && !isGithubAction) {
       console.log(`CLIENT: ${JSON.stringify(message)}`)
     }
-    return message;
+    return message
   })
-  serverConnection.forward(socketConnection, message => {
-    const prefix = isDevelopment ? projectsBasePath : ""
+  serverConnection.forward(socketConnection, (message) => {
+    const prefix = isDevelopment ? projectsBasePath : ''
     FilenamesToUri(prefix, message)
 
     if (message.params?.diagnostics) {
       message.params.diagnostics = message.params.diagnostics.flatMap((diag) => {
         if (diag?.message?.startsWith('__WORKBENCH__ ')) {
-          const json = diag.message.substring(14);
+          const json = diag.message.substring(14)
           if (sessionId) {
-            console.log("BENCH: " + json)
+            console.log('BENCH: ' + json)
           }
           sendToSession(sessionId, json)
           return []
@@ -235,16 +266,17 @@ wss.addListener("connection", async function(ws, req) {
 
     if (message.result?.flatMap) {
       message.result = message.result.flatMap((res) => {
-        if (res.message?.tag?.some(elem => elem.expr?.text?.startsWith("__WORKBENCH__ "))) return [];
-        return [res];
+        if (res.message?.tag?.some((elem) => elem.expr?.text?.startsWith('__WORKBENCH__ ')))
+          return []
+        return [res]
       })
     }
 
     if (isDevelopment && !isGithubAction) {
       console.log(`SERVER: ${JSON.stringify(message)}`)
     }
-    return message;
-  });
+    return message
+  })
 
   ws.on('close', () => {
     socketCounter -= 1
