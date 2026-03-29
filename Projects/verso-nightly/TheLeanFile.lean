d57c5504bc42a35a97bad7aa6501ceace60cfac3
Rob Simmons <rob@lean-fro.org>
2026-03-29 12:26:40 -0400

    Make sure example file includes VersoManual

--- Projects/verso-nightly/TheLeanFile.lean
+++ Projects/verso-nightly/TheLeanFile.lean
@@ -1,6 +1,26 @@
-/-! *bold* -/
-/-- _italics_ -/
-def x := 989 -- line
+import VersoManual
+import VersoBlog
 
+open Verso Doc
+open Verso.Genre Manual
+open Verso.Genre.Manual.InlineLean
 
-/- Normalr _italics_ . -/ 
+#doc (Manual) "My Document" =>
+
+This is a Verso document.
+
+It can include inline math, like this: $`x + 4 = -3`.
+
+It can also include block math, like
+$$`\int_\mathsf{this}^\mathtt{orthis} \mathit{or{\ldots}maybe{\ldots}this}`
+
+We also support inline lean, like this: {lean}`Nat.add_assoc`.
+
+We also support block lean, like this:
+
+```lean
+/-- The name we will be greeting -/
+def theName := "Jimothy"
+
+#eval s!"Hello {theName}"
+```
