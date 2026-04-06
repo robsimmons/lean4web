03733883520a13db89ebd624f7ad5b4c9fb9a8fc
Rob Simmons <rob@lean-fro.org>
2026-04-06 17:47:34 -0400

    add even more to starter file

--- Projects/MathlibDemo/TheLeanFile.lean
+++ Projects/MathlibDemo/TheLeanFile.lean
@@ -1,1 +1,34 @@
-def x := 4
+import Mathlib
+import ProofWidgets
+import Plausible
+
+import MathlibDemo.Bijection
+import MathlibDemo.Logic
+import MathlibDemo.Rational
+import MathlibDemo.Ring
+import VersoManual
+open Verso Doc
+open Verso.Genre Manual
+open Verso.Genre.Manual.InlineLean
+
+#doc (Manual) "My Document" =>
+
+This is a Verso document.
+
+It can include inline math, like this: $`x + 4 = -3`.
+
+It can also include block math, like
+$$`\int_\mathsf{this}^\mathtt{orthis} \mathit{or{\ldots}maybe{\ldots}this}`
+
+We also support inline lean, like this: {lean}`Nat.add_assoc`.
+
+We also support block lean, like this:
+
+```lean
+/-- The name we will be greeting -/
+def theName := "Jimothy"
+
+#eval s!"Hello {theName}"
+```
+
+this is the example
